3ea38052feaa0da7f9297df03c4a668522a61f8f
yescas <yescasjulio7@gmail.com>
2024-01-19 15:53:07 -0600

    functionn on input component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+import AddCategory from "./components/AddCategory";
 export default function GifExpertApp() {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  const onAddCategory = () => {
+    //Valorant
+    setCategories([...categories, "Valorant"]);
+  };
+
+  //   console.log(categories);
   return (
     <>
       <h1>GifExpertApp</h1>
+      <AddCategory />
+      <button onClick={onAddCategory}>Agregar</button>
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+        {/* <li>ABC</li> */}
+      </ol>
     </>
   );
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function AddCategory() {
+  const [inputValue, setInputValue] = useState("OnePunch");
+
+  const onInputChange = ({ target }) => {
+    setInputValue(target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+  };
+
+  return (
+    <>
+      <form onSubmit={(event) => onSubmit(event)}>
+        <input
+          type="text"
+          placeholder="Buscar gifs"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+      </form>
+    </>
+  );
+}
